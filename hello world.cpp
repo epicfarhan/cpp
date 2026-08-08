91b4f6d08d8949767e8a4b10dbce61ee0745581e
unknown <fnasim1102@gmail.com>
2026-08-09 00:00:00 +0600

    basic data types

--- hello world.cpp
+++ hello world.cpp
@@ -2,8 +2,29 @@
 
 int main(){
 
+    // int
     int x;
     x = 5;
-    std::cout << x;
+
+    // decimal
+    double y = 5.53;
+
+    // strings or characters
+
+    char grade = 'A';
+
+    // boolean
+
+    bool power = false;
+
+    // sting
+
+    std::string name = "farhan";
+
+    std::cout << x << "\n";
+    std::cout << y << "\n";
+    std::cout << grade << "\n";
+    std::cout << power << "\n";
+    std::cout << "hello " << name;
     return 0;
 }
