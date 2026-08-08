e2daa3b022013a40e387770755932f4cfbed2516
unknown <fnasim1102@gmail.com>
2026-08-09 02:39:58 +0600

    strings, namespace, const

--- hello world.cpp
+++ hello world.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+//namespaces
+namespace info{
+    int age = 20;
+    std::string gender = "male";
+}
+
+namespace status{
+    int health = 100;
+    std::string items = "sword";
+}
+
+// main function
 
 int main(){
+
+    using namespace status;
 
     // int
     int x;
@@ -21,10 +35,19 @@ int main(){
 
     std::string name = "farhan";
 
+    // const
+
+    const std::string lastname = " nasim";
+    
+
     std::cout << x << "\n";
     std::cout << y << "\n";
     std::cout << grade << "\n";
     std::cout << power << "\n";
-    std::cout << "hello " << name;
+    std::cout << "hello " << name  << lastname << "\n";
+    std::cout << "age : " << info::age << "\n";
+    std::cout << "health : " << health << "\n";
+    std::cout << "items : " << items << "\n";
+
     return 0;
 }
