12eece5a6806b8da0805180e32b326f7cf7d7372
unknown <fnasim1102@gmail.com>
2026-08-10 05:15:14 +0600

    data types and math functions

--- hello world.cpp
+++ hello world.cpp
@@ -53,12 +53,61 @@ int main(){
     // typedef example
 
     const text_t fruit = "peaches";
-    const number_t productNumber = 12;
+    number_t productNumber = 12;
 
-    // add
+    // addition
+
+    number_t numOne = 1;
+    numOne = numOne + 2; // += works too, ++ adds 1
+    
+    // sub
+
+    number_t numTwo = 10;
+    numTwo = numTwo - 4; // -= works as well,  --  subs 1
+
+   // multiplication
+
+   number_t numThree = 3;
+   numThree = numThree * 3; // *= , ** or *<number> doubles
+
+   // divide 
+
+   number_t numFour = 4;
+   numFour = numFour / 4; // /= 
+
+   //remainder
+
+   number_t numberFive = 5;
+   numberFive = numberFive % 2; // %=
+
+
+  // type conversion
+
+   double decimal = (int) 4.13; // converted to int
+   
+   cout << (char) 100 << "\n"; // prints d cause the converted value of 100 in string is d;
+
+   int correct = 8;
+   int questions = 10;
+   double result = (double) correct / (double)questions * 100;
+   cout << result << "%" << "\n";
 
 
 
+   // inputs
+
+   text_t username;
+
+   cout << "What is your name?" << "\n";
+   cin >> username;
+   cout << "hello " << username << "\n";
+
+   // using nextline inputs 
+   text_t fullname;
+   cout << "What is fullname?" << "\n";
+   getline(cin >> ws, fullname);// auto writes new line
+
+   // outputs for testing
     cout << x << "\n";
     cout << y << "\n";
     cout << grade << "\n";
@@ -67,7 +116,10 @@ int main(){
     cout << "age : " << info::age << "\n";
     cout << "health : " << health << "\n";
     cout << "items : " << items << "\n";
-    cout << fruit << " : " << productNumber;
+    cout << fruit << " : " << productNumber << "\n";
+    cout << numOne << "\n";
+    cout << numTwo << "\n";
+    cout << decimal;
 
     return 0;
 }
---
+++ math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+int  main(){
+  using namespace std;
+
+  double x = 1.2;
+  double y = 3.19;
+  double z;
+
+  //z = max(x, y); // gives the biggest number 
+  //z = min(x, y); // gives the smallest number
+  //z = pow(2, 4); // raises to the number of 4 
+  //z = sqrt(9); //squre root of 9
+  //z = abs(-3); // turns negative to positive
+  //z = round(y); // rounds number
+  //z = ceil(y); // raises number to highest
+  //z = floor(y); // lowers the number
+  cout << z;
+  
+ 
+
+  return 0;
+}
